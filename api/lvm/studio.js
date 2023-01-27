f6b719a24d1a1a3e333e027af8afc1d3fbbcaf73
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2023-01-26 20:02:53 -0500

    Update studio.js

--- api/lvm/studio.js
+++ api/lvm/studio.js
@@ -21,72 +21,44 @@ function toObjectString(attrs, params) {
 function tutorialStarted() {}
 function tutorialStep(sn) {}
 function tutorialCompleted() {
-  $.ajax({
-    type: 'POST',
-    url: '/ajax/tutorialStatus/completed'
-  });
+	$.ajax({
+		type: 'POST',
+		url: '/ajax/tutorialStatus/completed'
+	});
 }
 // setup interactive tutorial
 fetch('/ajax/getTutorialShowStatus').then(status => {
-  interactiveTutorial.isShowTutorial = status ? false : true;
+	interactiveTutorial.isShowTutorial = status ? false : true;
 }).catch(e => console.log(e));
 // get some params not included in the flashvars.
 function get(type) {
-  fetch(`/ajax/getParams?${type}=true`).then(info => {
-  }).catch(e => console.log(e));
+	fetch(`/ajax/getParams?${type}=true`).then(info => {
+		return info;
+	}).catch(e => console.log(e));
 }
 function initPreviewPlayer(dataXmlStr, startFrame) {
-  if (typeof startFrame == 'undefined') startFrame = 1;
-  else startFrame = Math.max(1, parseInt(startFrame));
-  if (dataXmlStr === null) return;
-  const attrs = {
-    height: 360,
-    width: 640,
-    player_url: get("animationPath") + "/player.swf",
-    quality: "medium"
-  }; 
-  const params = {
-    movieOwner: "", 
-    movieOwnerId: "", 
-    movieId: flashvars.movieId || "", 
-    ut: "-1",
-    movieLid: "8", 
-    movieTitle: "", 
-    movieDesc: "", 
-    userId: "", 
-    username: "", 
-    uemail: "",
-    apiserver: "https://gowdpk.ga/", 
-    thumbnailURL: "", 
-    copyable: "0", 
-    isPublished: "0", 
-    ctc: "go", 
-    tlang: "en_US", 
-    is_private_shared: "0",
-    autostart: "1", 
-    appCode: "go", 
-    is_slideshow: "0", 
-    originalId: "0", 
-    is_emessage: "0", 
-    isEmbed: "0", 
-    refuser: "",
-    utm_source: "", 
-    uid: "", 
-    isTemplate: "1", 
-    showButtons: "0", 
-    chain_mids: "", 
-    showshare: "0", 
-    averageRating: "",
-    s3base: "https://gowdpk.ga/movie_thumbs/",
-    ratingCount: "", 
-    fb_app_url: "https://gowdpk.ga/", 
-    numContact: 0, 
-    isInitFromExternal: 1, 
-    storePath: flashvars.storePath, 
-    clientThemePath: flashvars.clientThemePath, 
-    animationPath: get("animationPath") + "/",
-    startFrame: startFrame
-  };
-  document.getElementById('previewWindow').style.display='block';
-  document.getElementById('playerdiv').innerHTML = toObjectString(attrs, params);
+	if (typeof startFrame == 'undefined') startFrame = 1;
+	else startFrame = Math.max(1, parseInt(startFrame));
+	if (dataXmlStr === null) return;
+	const attrs = {
+		height: 360,
+		width: 640,
+		data: get("animationPath") + "/player.swf",
+		quality: "medium",
+		type: "application/x-shockwave-flash",
+		allowScriptAccess: "always"
+	}; 
+	const params = {
+		apiserver: "/",
+		isEmbed: 1,
+		tlang: "en_US",
+		isInitFromExternal: 1,
+		startFrame: startFrame,
+		autostart: 1,
+		storePath: flashvars.storePath, 
+		clientThemePath: flashvars.clientThemePath, 
+		animationPath: get("animationPath") + "/",
+	};
+	document.getElementById('previewWindow').style.display='block';
+	document.getElementById('playerdiv').innerHTML = toObjectString(attrs, params);
 }
