2dd912d4e9189b7a5446f3268e31beda69b2b2ba
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2023-01-27 17:33:58 -0500

    Update lvm.js

--- api/lvm.js
+++ api/lvm.js
@@ -10,23 +10,23 @@ var previewPlayerTempData = "";
 /// Previewer
 ///
 function initPreviewPlayer(dataXmlStr, startFrame, containsChapter, themeList) {
-  // New variable to be used by loadPreviewer()
+	// New variable to be used by loadPreviewer()
 	movieDataXmlStr = dataXmlStr;
 	// Movie XML
 	filmXmlStr = dataXmlStr.split("<filmxml>")[1].split("</filmxml>")[0];
 	// setup preview popup
-  $("#playerdiv").append(`<iframe style="border: 0px; width: 870px; height: 420px;" src="/player?isInitFromExternal=1&startFrame=${startFrame}"></iframe>`);
+	$("#playerdiv").append(`<iframe style="border: 0px; width: 870px; height: 420px;" src="/player?isInitFromExternal=1&startFrame=${startFrame}"></iframe>`);
 	$("#player-modal").show();
-  // Load the Video Previewer
-  loadPreviewer();
+	// Load the Video Previewer
+	loadPreviewer();
 }
 function setupTutorial() {
-  fetch('/ajax/getTutorialShowStatus').then(status => {
-    return status;
-  });
+	fetch('/ajax/getTutorialShowStatus').then(status => {
+		return status;
+	});
 }
 function loadPreviewer() {
-  // I think this is in case of an error??
+	// I think this is in case of an error??
 	if (movieDataXmlStr === null) return;
 	// I don't know
 	savePreviewData(movieDataXmlStr);
@@ -42,25 +42,13 @@ interactiveTutorial.isShowTutorial = setupTutorial();
 function tutorialStarted() {}
 function tutorialStep(sn) {}
 function tutorialCompleted() {
-  $.ajax({
-    type: 'POST',
-    url: '/ajax/tutorialStatus/completed'
-  });
+	$.ajax({
+		type: 'POST',
+		url: '/ajax/tutorialStatus/completed'
+	});
 }
 // Hide Video Previewer popup
 function hidePreviewer() {
-  $("#playerdiv").append(``);
-  $("#player-modal").hide();
-}
-function hideTutorial() {
-  $("#video-tutorial").hide();
-}
-var videoTutorial = new VideoTutorial($("#video-tutorial"));
-VideoTutorial.tutorials.composition = {
-  title: 'Composition Tutorial',
-  wistiaId: 'nuy96pslyp',
-};
-VideoTutorial.tutorials.enterexit = {
-  title: 'Enter and Exit Effects Tutorial',
-  wistiaId: 'fvjsa3jnzc',
+	$("#playerdiv").append(``);
+	$("#player-modal").hide();
 }
